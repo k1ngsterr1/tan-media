0728859820ede54ca2ceb2d9619248332719224b
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2024-03-02 15:24:08 +0600

    Version 1.2.2

--- src/widgets/Screens/Resources/index.tsx
+++ src/widgets/Screens/Resources/index.tsx
@@ -19,46 +19,46 @@ export const ResourcesScreen = () => {
         <ResourceTab
           name="Business FM"
           bgImage="businessFM hoverable"
-          href="https://forbes.kz/"
+          href="https://businessfm.kz/"
         />
         <ResourceTab
           name="Arbat Media"
           bgImage="arbat hoverable"
-          href="https://forbes.kz/"
+          href="https://arbatmedia.kz/"
         />
       </div>
       <div className="w-full flex justify-between items-start mt-8">
         <ResourceTab
           name="Muzzone"
           bgImage="muzzone hoverable"
-          href="https://forbes.kz/"
+          href="https://www.instagram.com/muzzone.tv/"
         />
         <ResourceTab
           name="Tan Digital Media"
           bgImage="tan_digital hoverable"
-          href="https://forbes.kz/"
+          href="https://tanmediagroup.kz/"
         />
         <ResourceTab
           name="@Armanzhan"
           bgImage="armanzhan hoverable"
-          href="https://forbes.kz/"
+          href="https://www.instagram.com/armanzhan/"
         />
       </div>
       <div className="w-full flex justify-between items-start mt-8">
         <ResourceTab
           name="Baitassov Live"
           bgImage="baitassov_live hoverable"
-          href="https://forbes.kz/"
+          href="https://www.youtube.com/channel/UCh0k3JlUGI0UBFQOUPaaImQ"
         />
         <ResourceTab
           name="Mediacafe"
           bgImage="mediacafe hoverable"
-          href="https://forbes.kz/"
+          href="https://tanmediagroup.kz/uniquemedia"
         />
         <ResourceTab
           name="Unique Media"
           bgImage="unique hoverable"
-          href="https://forbes.kz/"
+          href="https://tanmediagroup.kz/mediacafe"
         />
       </div>
     </section>
